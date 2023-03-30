9002217de604a855ba329a18fe30c177a90e17e2
jashankaur05 <128462711+jashankaur05@users.noreply.github.com>
2023-03-30 02:07:40 -0700

    Add files via upload

---
+++ trycatch.js
@@ -0,0 +1,33 @@
+// //1
+//   try {
+
+//     var x = y + 1; 
+//     console.log(x)
+    
+//   } catch (error) {
+//     console.log("Error message:", error.message);
+//   } finally {
+//     console.log("Finally block started");
+//     console.log("Finally block ended");
+//   }
+
+
+  //error  7  types 
+//1 syntax error missing semicolon , parenthese  and curly brackets
+// x=10
+// if (x==10){
+//   console.log('yes')
+// }
+  
+//2 referece error  variable and function has not been defined 
+//console.log(get)
+
+//type error wrong data type 
+// x='hello';
+// console.log(x.push('world'))
+// num=15
+// console.log(num.split(' '))
+let x=10
+ let y=20
+ let  z=x
+console.log(x,y,z)
